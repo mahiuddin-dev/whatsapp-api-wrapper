e2d01a7b24dec0b940448e0514e432d2efaf215e
mahiuddin-dev <codermahiuddin@gmail.com>
2025-01-10 21:52:47 +0600

    Update readme and requirements

--- setup.py
+++ setup.py
@@ -1,8 +1,5 @@
 from setuptools import setup, find_packages
 
-with open("requirements.txt") as f:
-    requirements = f.read().splitlines()
-
 setup(
     name="whatsapp_api",
     version="0.1.0",
@@ -12,5 +9,7 @@ setup(
     packages=find_packages(where="src"),
     package_dir={"": "src"},
     python_requires=">=3.8",
-    install_requires=requirements,
+    install_requires=[
+        "requests>=2.32.3"
+    ],
 )
